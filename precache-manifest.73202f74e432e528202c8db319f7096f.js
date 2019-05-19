c7d3da4d0dc0d8224359adc83745e39004b1bea0
Amol Naval <amolnava@amdocs.com>
2019-05-19 19:38:20 +0530

    Updates

---
+++ precache-manifest.73202f74e432e528202c8db319f7096f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f633ad3cee90793e9295",
+    "url": "/email-dashboard/static/js/runtime~main.801db0b2.js"
+  },
+  {
+    "revision": "9e305722c776d47848b6",
+    "url": "/email-dashboard/static/js/main.3445fe23.chunk.js"
+  },
+  {
+    "revision": "cf174bde31b33ab8521d",
+    "url": "/email-dashboard/static/js/2.482c8c3f.chunk.js"
+  },
+  {
+    "revision": "9e305722c776d47848b6",
+    "url": "/email-dashboard/static/css/main.685da79a.chunk.css"
+  },
+  {
+    "revision": "4c04e968c9c6c78aa8453855be098e29",
+    "url": "/email-dashboard/index.html"
+  }
+];
